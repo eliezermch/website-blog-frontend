55a56ea11a4bae61816e53e04049cb6889516385
Eliezer Chirino <emchch3001@gmail.com>
2024-05-09 22:09:44 -0300

    Update: develop task card & task modal

--- src/components/task-card.tsx
+++ src/components/task-card.tsx
@@ -1,13 +1,29 @@
-import { Card, CardBody } from '@nextui-org/react';
+'use client';
+
+import { Button, Card, CardBody, useDisclosure } from '@nextui-org/react';
+import { IconEdit, IconTrash } from '@tabler/icons-react';
+import TaskModal from './task-modal';
 
 const TaskCard = () => {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
   return (
-    <Card className="bg-green-700 mb-[12px]">
-      <CardBody>
-        <h4>tilte</h4>
-        <p>Make beautiful websites regardless of your design experience.</p>
-      </CardBody>
-    </Card>
+    <>
+      <Card className="bg-green-700 mb-[12px] hover:cursor-pointer">
+        <CardBody onClick={onOpen} className="max-h-[100px] min-h-[100px]">
+          <div className="flex justify-between">
+            <h4>tilte</h4>
+            <div className="flex gap-[8px]">
+              <IconEdit onClick={onOpen} className="w-5 h-5 hover:cursor-pointer hover:opacity-[0.8]" />
+
+              <IconTrash className="w-5 h-5 hover:text-rose-700 hover:cursor-pointer" />
+            </div>
+          </div>
+          <p>Make beautiful websites regardless of your design experience.</p>
+        </CardBody>
+      </Card>
+      <TaskModal isOpen={isOpen} onOpen={onOpen} onOpenChange={onOpenChange} />
+    </>
   );
 };
 
---
+++ src/components/task-modal.tsx
@@ -0,0 +1,55 @@
+import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button } from '@nextui-org/react';
+
+interface Props {
+  isOpen: boolean;
+  onOpen: () => void;
+  onOpenChange: () => void;
+}
+
+export default function TaskModal({ onOpen, isOpen, onOpenChange }: Props) {
+  return (
+    <>
+      <Modal
+        backdrop="blur"
+        isOpen={isOpen}
+        size="2xl"
+        classNames={{
+          backdrop: 'bg-green-900/20 backdrop-opacity-40',
+        }}
+        onOpenChange={onOpenChange}
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">Modal Title</ModalHeader>
+              <ModalBody>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pulvinar risus non risus hendrerit
+                  venenatis. Pellentesque sit amet hendrerit risus, sed porttitor quam.
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pulvinar risus non risus hendrerit
+                  venenatis. Pellentesque sit amet hendrerit risus, sed porttitor quam.
+                </p>
+                <p>
+                  Magna exercitation reprehenderit magna aute tempor cupidatat consequat elit dolor adipisicing. Mollit
+                  dolor eiusmod sunt ex incididunt cillum quis. Velit duis sit officia eiusmod Lorem aliqua enim laboris
+                  do dolor eiusmod. Et mollit incididunt nisi consectetur esse laborum eiusmod pariatur proident Lorem
+                  eiusmod et. Culpa deserunt nostrud ad veniam.
+                </p>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="light" onPress={onClose}>
+                  Close
+                </Button>
+                <Button color="primary" onPress={onClose}>
+                  Action
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
