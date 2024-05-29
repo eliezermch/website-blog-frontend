64ece1487bc34ce0dc0e15251cb97f493bcbc1d6
Eliezer Chirino <emchch3001@gmail.com>
2024-05-29 15:35:05 -0300

    add layout for the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { getSession } from '@/utils/session';
+import { Card, CardHeader, Image, Link } from '@nextui-org/react';
 import { redirect } from 'next/navigation';
 
 export default async function Home() {
@@ -9,8 +10,52 @@ export default async function Home() {
   }
 
   return (
-    <main className="dark">
-      <pre>{JSON.stringify(session, null, 2)}</pre>
+    <main className="dark flex flex-col justify-start items-center w-full">
+      <div className="mt-[60px]">
+        <h1 className="text-[50px]">Welcome to your Space</h1>
+      </div>
+
+      <div className="bg-transparent relative w-[80%] h-[60%] mt-[60px] gap-2 grid grid-cols-12 gap-x-10">
+        <Card as={Link} href={'/profile'} shadow="md" isPressable className="col-span-12 sm:col-span-4 h-[400px]">
+          <CardHeader className="absolute z-10 top-1 flex-col !items-start">
+            <p className="text-tiny text-[#45D483] uppercase font-bold">What to watch</p>
+            <h4 className="text-white font-medium text-large">Stream the Acme event</h4>
+          </CardHeader>
+          <Image
+            isZoomed
+            removeWrapper
+            alt="Card background"
+            className="z-0 w-full h-full object-cover"
+            src="https://nextui.org/images/card-example-4.jpeg"
+          />
+        </Card>
+        <Card as={Link} href={'/tasks'} shadow="md" isPressable className="col-span-12 sm:col-span-4 h-[400px]">
+          <CardHeader className="absolute z-10 top-1 flex-col !items-start">
+            <p className="text-tiny text-[#45D483] uppercase font-bold">Plant a tree</p>
+            <h4 className="text-white font-medium text-large">Contribute to the planet</h4>
+          </CardHeader>
+          <Image
+            isZoomed
+            removeWrapper
+            alt="Card background"
+            className="z-0 w-full h-full object-cover"
+            src="https://nextui.org/images/card-example-3.jpeg"
+          />
+        </Card>
+        <Card as={Link} href={'/myblog'} shadow="md" isPressable className="col-span-12 sm:col-span-4 h-[400px]">
+          <CardHeader className="absolute z-10 top-1 flex-col !items-start">
+            <p className="text-tiny text-[#45D483] uppercase font-bold">Supercharged</p>
+            <h4 className="text-white font-medium text-large">Creates beauty like a beast</h4>
+          </CardHeader>
+          <Image
+            isZoomed
+            removeWrapper
+            alt="Card background"
+            className="z-0 w-full h-full object-cover"
+            src="https://nextui.org/images/card-example-2.jpeg"
+          />
+        </Card>
+      </div>
     </main>
   );
 }
--- src/components/todo-list.tsx
+++ src/components/todo-list.tsx
@@ -1,6 +1,6 @@
 import { Session, Task } from '@/types/session-model';
 import TaskCard from './task-card';
-import { ScrollShadow } from '@nextui-org/react';
+import { Card, ScrollShadow } from '@nextui-org/react';
 import { NewTaskButton } from './new-task-button';
 
 interface Props {
@@ -12,7 +12,10 @@ interface Props {
 
 const ToDoList = ({ title, data, tasks, id }: Props) => {
   return (
-    <div className="w-full relative max-w-[420px] min-w-[280px] h-[540px] p-[1.2rem] bg-zinc-800 rounded-small shadow-card transition-all duration-[0.2s] z-[1] ease-in-out">
+    <Card
+      shadow="lg"
+      className="w-full relative max-w-[420px] min-w-[280px] h-[540px] p-[1.2rem] bg-zinc-900/70 rounded-small shadow-card transition-all duration-[0.2s] z-[1] ease-in-out"
+    >
       <ScrollShadow hideScrollBar className="w-full h-full">
         <div className="w-full h-full">
           <div className="w-full flex justify-between mb-[12px]">
@@ -34,7 +37,7 @@ const ToDoList = ({ title, data, tasks, id }: Props) => {
           ))}
         </div>
       </ScrollShadow>
-    </div>
+    </Card>
   );
 };
 
